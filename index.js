9641f54d4132c173710daf48e6075fc9078974d0
e <jannatul0040@gmail.com>
2023-11-15 13:16:33 +0600

    created basic setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('boss is sitting')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Bistro boss is sitting on port ${port}`);
+  })
+
+  /**
+ * --------------------------------
+ *      NAMING CONVENTION
+ * --------------------------------
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('/users/:id')
+ * app.delete('/users/:id')
+ * 
+*/
