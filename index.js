ccf1dcff10d8319c01b83036f0ca9572f3468f1c
e <jannatul0040@gmail.com>
2023-11-19 23:11:18 +0600

    add to cart count using refetch

--- index.js
+++ index.js
@@ -52,7 +52,9 @@ async function run() {
 
       //data load
       app.get('/carts', async (req, res) => {
-        const result = await cartCollection.find().toArray();
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
         res.send(result);
       });
       
