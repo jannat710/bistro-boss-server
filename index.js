2895a588803254434ffeb387b7d90652e5ac3a5c
e <jannatul0040@gmail.com>
2023-11-20 15:15:49 +0600

    Delete cart item

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvt8fts.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -58,6 +58,13 @@ async function run() {
         res.send(result);
       });
       
+//delete cart item
+app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await cartCollection.deleteOne(query);
+    res.send(result); 
+})
 
 
     await client.db("admin").command({ ping: 1 });
