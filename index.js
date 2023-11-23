4975d0754787fe4bc6b3d6002cab688d93dd39db
e <jannatul0040@gmail.com>
2023-11-23 10:54:22 +0600

    Make user api secure using verify Admin

--- index.js
+++ index.js
@@ -56,9 +56,21 @@ async function run() {
             })
         }
 
+           // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        const isAdmin = user?.role === 'admin';
+        if (!isAdmin) {
+          return res.status(403).send({ message: 'forbidden access' });
+        }
+        next();
+      }
+
         // users related api
         //data load
-        app.get('/users', verifyToken, async (req, res) => {
+        app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
 
             const result = await userCollection.find().toArray();
             res.send(result);
@@ -67,7 +79,7 @@ async function run() {
         app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
             if (email !== req.decoded.email){
-                return res.status(403).send({ message: 'unauthorized access' })
+                return res.status(403).send({ message: 'forbidden access' })
             }
             const query = { email: email };
             const user = await userCollection.findOne(query);
@@ -94,7 +106,7 @@ async function run() {
         })
 
         //Admin
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', verifyToken,verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
@@ -106,7 +118,7 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id',verifyToken,verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
