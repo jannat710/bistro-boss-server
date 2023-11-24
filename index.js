7208672b13231b68865500195ba5dcf3d85064e4
e <jannatul0040@gmail.com>
2023-11-24 16:49:17 +0600

    get payment data

--- index.js
+++ index.js
@@ -162,6 +162,16 @@ async function run() {
             res.send({ paymentResult, deleteResult });
           })
 
+
+          app.get('/payments/:email', verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            if (req.params.email !== req.decoded.email) {
+              return res.status(403).send({ message: 'forbidden access' });
+            }
+            const result = await paymentCollection.find(query).toArray();
+            res.send(result);
+          })
+
         //menus data load
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
